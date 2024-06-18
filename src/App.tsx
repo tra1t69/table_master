389a557c54d250fef6ac4e3787e73db979506c95
Ivan Nukulin <nik.tra1t@gmail.com>
2024-06-19 03:37:32 +0700

    createUser

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import './App.css';
 import LoginPage from './components/pages/login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CreateTablePage from './components/pages/createTable';
+import CreateUserPage from './components/pages/createUser';
 
 const App:FC = () =>{
   return (
     <div>
-      <CreateTablePage />
+      <CreateUserPage />
     </div>
   );
 }
---
+++ src/components/pages/createUser.tsx
@@ -0,0 +1,54 @@
+import React, { FC, FormEvent, useState, ChangeEvent } from "react";
+import Header from "./header";
+import { Form, Button, Container } from "react-bootstrap";
+import './createUser.css'
+const CreateUserPage:FC = () =>{ 
+  const[userName, setUserName] = useState('');
+  const[password, setPassword] = useState('');
+
+  const onSubmit  = (e: FormEvent) => {
+    e.preventDefault();
+  }
+  const onUserName = (e: ChangeEvent<HTMLInputElement>) =>{
+    e.preventDefault();
+    setUserName(e.target.value);
+  }
+
+  const onPassword = (e: ChangeEvent<HTMLInputElement>) =>{
+    e.preventDefault();
+    setPassword(e.target.value);
+  }
+
+  return(
+    <div>
+    <Header />
+    <Container className="mt-5">
+    <Form onSubmit={onSubmit}  className="form d-flex flex-column justify-content-center">
+    <Form.Group className="mb-2">
+      <Form.Label>Логин</Form.Label>
+      <Form.Control
+      type="text"
+      name="table-name"
+      id="table-name"
+      onChange={onUserName}
+      />
+    </Form.Group>
+    <Form.Group className="mb-2">
+      <Form.Label>Пароль</Form.Label>
+      <Form.Control
+      type="text"
+      name="column"
+      id="column"
+      onChange={onPassword}
+      />
+    </Form.Group>
+    <Button className="form-button" type="submit">Создать</Button>
+    </Form>
+    </Container>
+
+
+    </div>
+  )
+}
+
+export default CreateUserPage;
